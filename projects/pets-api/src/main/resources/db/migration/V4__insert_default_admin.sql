ac28c181e46a85d02e8df2f5c0624b6dc106c53c
gyobycoders <gyowanny.queiroz@bycoders.com.br>
2026-01-08 11:15:23 -0300

    Added projects folder to gitignore

--- projects/pets-api/src/main/resources/db/migration/V4__insert_default_admin.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (username, email, password, role, created_at, updated_at)
-VALUES ('admin', 'admin@pets.com', '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9lBOsl7iwK8pJ5C', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-ON CONFLICT (username) DO NOTHING;
